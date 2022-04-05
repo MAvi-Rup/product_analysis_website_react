ec6b1c8a464c26618911cdd5baabf6b429272c56
MAvi-Rup <avirupmondal19@gmail.com>
2022-04-05 20:12:22 +0600

    Review Page Done

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
+import CustromerReview from '../CustomerReview/CustromerReview';
 
 const Review = () => {
+    const [products,setProducts] = useProducts()
     return (
-        <div>
-            <h1>Review Componenrt</h1>
+        <div className='container'>
+            <h1 className='text-info'>Here All Our Customer Review</h1>
+            <div className='d-flex flex-wrap'>
+            {
+                products.map(product =><CustromerReview key={product.id} product={product}></CustromerReview>)
+            }
+
+            </div>
         </div>
     );
 };
